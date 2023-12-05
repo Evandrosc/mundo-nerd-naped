abd3aaae9328a6503bc5c25ff7715e009c0cedc4
Evandrosc <evandromdsc@gmail.com>
2023-12-05 15:30:06 -0300

    feat: main

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,8 @@
+export function Main () {
+  return (
+    <main>
+      <h1>Mundo nerd? Naped!</h1>
+      <p>O Naped pode ser sua fonte de informações sobre o mundo nerd e outros assuntos relacionados.</p>
+    </main>
+  );
+}
