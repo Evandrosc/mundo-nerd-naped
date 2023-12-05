910f1203375708ca550d3f46cc4b924562d6c578
Evandrosc <evandromdsc@gmail.com>
2023-12-05 15:30:19 -0300

    feat: header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <header>
+      <span>Naped</span>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Séries</a>
+        <a href="">Filmes</a>
+        <a href="">Animes</a>
+        <a href="">Games</a>
+      </nav>
+      <button>Minha conta</button>
+    </header>
+  );
+}
