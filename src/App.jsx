7cdd4e8a8ed0b2b2a25aac81f2f604d309621445
Evandrosc <evandromdsc@gmail.com>
2023-12-05 16:42:13 -0300

    feat: header, main, footer no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { GlobalStyle } from './styles/GlobalStyle'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+import { Footer } from './components/Footer'
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
     </ThemeProvider>
   )
 }
