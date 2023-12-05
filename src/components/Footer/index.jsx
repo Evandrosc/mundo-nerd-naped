6dd35c35780fe49a36c16a36807f5f4434aeebb4
Evandrosc <evandromdsc@gmail.com>
2023-12-05 15:29:24 -0300

    feat: footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+export function Footer () {
+  return (
+    <footer>
+      <span>Naped</span>
+      <p>Todas as imagens de filmes, séries e etc são marcas registradas dos seus respectivos proprietários.</p>
+    </footer>
+  );
+}
