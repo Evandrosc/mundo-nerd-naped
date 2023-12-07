82ba6588a41133f7d802e697a26bb8802451ebd1
Evandrosc <evandromdsc@gmail.com>
2023-12-07 13:52:58 -0300

    feat: estilo scrollbar

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -26,4 +26,41 @@ export const GlobalStyle = createGlobalStyle`
   p, a {
     font-family: ${props => props.theme.inter};
   }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${props => props.theme.brandColor};
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${props => props.theme.dark20};
+  }
+
+  @media screen and (max-width: 482px) {
+    ::-webkit-scrollbar {
+      width: 4px;
+    }
+  }
+
+  /* Para navegadores baseados no motor Gecko (Firefox) */
+  scrollbar {
+    width: 8px;
+  }
+
+  thumb {
+    background-color: ${props => props.theme.brandColor};
+  }
+
+  track {
+    background-color: ${props => props.theme.dark20};
+  }
+
+  @media screen and (max-width: 482px) {
+    scrollbar {
+      width: 4px;
+    }
+  }
 `
