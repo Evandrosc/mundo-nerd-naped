c18c86d69a4402329a028363ebdd9493a4a26802
Evandrosc <evandromdsc@gmail.com>
2023-12-20 16:35:06 -0300

    feat: react router dom e global styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,66 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
+import { Home } from './page/Home'
+import { ListPosts } from './page/ListPosts'
+import { Post } from './page/Post'
+import { Error } from './components/Error'
+
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/GlobalStyle.js'
+import { defaultTheme } from './styles/themes/default.js'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/Séries',
+        element: <ListPosts gender='Séries' />,
+      },
+      {
+        path: '/filmes',
+        element: <ListPosts gender='Filmes' />,
+      },
+      {
+        path: '/Animes',
+        element: <ListPosts gender='Animes' />,
+      },
+      {
+        path: '/Games',
+        element: <ListPosts gender='Games' />,
+      },
+      {
+        path: '/Séries/:id',
+        element: <Post />,
+      },
+      {
+        path: '/Filmes/:id',
+        element: <Post />,
+      },
+      {
+        path: '/Animes/:id',
+        element: <Post />,
+      },
+      {
+        path: '/Games/:id',
+        element: <Post />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
