ec8239d6ca117f5e998639ba0f1594496894cd39
Evandrosc <evandromdsc@gmail.com>
2023-12-20 16:32:31 -0300

    feat: navigation

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,53 @@
+import { btnPrevPosts } from '../../assets/btnPrevPosts.jsx'
+import { btnNextPosts } from '../../assets/btnNextPost.jsx'
+import { Button, SectionContainer } from './styles'
+
+const MAX_ITEMS = 9
+const MAX_LEFT = (MAX_ITEMS - 1) / 2
+
+export function Pagination({
+  limit,
+  total,
+  offset,
+  setOffset,
+}) {
+  const current = offset ? offset / limit + 1 : 1
+  const pages = Math.ceil(total / limit)
+  const maxFirst = Math.max(pages - (MAX_ITEMS - 1), 1)
+  const first = Math.min(
+    Math.max(current - MAX_LEFT, 1),
+    maxFirst
+  )
+
+  function onPageChange(page) {
+    setOffset((page - 1) * limit)
+  }
+
+  return (
+    <SectionContainer>
+      <Button
+        onClick={() => onPageChange(current - 1)}
+        disabled={current === 1}
+      >
+        {btnPrevPosts}
+      </Button>
+      {Array.from({ length: Math.min(MAX_ITEMS, pages) })
+        .map((_, index) => index + first)
+        .map((page) => (
+          <Button
+            key={page}
+            onClick={() => onPageChange(page)}
+            $active={page === current}
+          >
+            {page}
+          </Button>
+        ))}
+      <Button
+        onClick={() => onPageChange(current + 1)}
+        disabled={current === pages}
+      >
+        {btnNextPosts}
+      </Button>
+    </SectionContainer>
+  )
+}
