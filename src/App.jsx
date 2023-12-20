f40df6a248ffa2efc1ab93b540845571090fac71
Evandrosc <evandromdsc@gmail.com>
2023-12-20 16:30:58 -0300

    feat: api no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { GlobalStyle } from './styles/GlobalStyle'
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Header } from './components/Header'
 import { Main } from './components/Main'
 import { Footer } from './components/Footer'
 
-export function App() {
+const queryClient = new QueryClient()
 
+export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+    <QueryClientProvider client={queryClient}>
       <Header />
       <Main />
       <Footer />
-    </ThemeProvider>
+    </QueryClientProvider>
   )
 }
